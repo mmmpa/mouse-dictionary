86e843fff3055eabe5f0eaeaa27f863021e2ba77
wtetsu <tewatana@gmail.com>
2019-01-02 07:03:51 +0900

    Remove unused files

--- static/frame.js
+++
@@ -1,7 +0,0 @@
-if (window !== window.parent) {
-  document.body.addEventListener("mousemove", e => {
-    //console.info(e);
-    chrome.runtime.sendMessage({ text: "test", e });
-    //chrome.tabs.sendRequest(tab.id, request);
-  });
-}
