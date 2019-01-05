ce670fa2375604e688aaee9aeb84617feafdb52d
wtetsu <tewatana@gmail.com>
2019-01-05 23:46:07 +0900

    Remove unused codes

--- static/background.js
+++ static/background.js
@@ -2,17 +2,6 @@ chrome.browserAction.onClicked.addListener(() => {
   chrome.tabs.executeScript({
     file: "./main.js"
   });
-
-  chrome.tabs.executeScript({
-    file: "./frame.js",
-    allFrames: true
-  });
-});
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, message);
-  });
 });
 
 chrome.runtime.onConnectExternal.addListener(function(receivePort) {
